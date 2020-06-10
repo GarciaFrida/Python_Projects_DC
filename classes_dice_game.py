aa279b1215a6e9848d67cdc80243540a618d05c4
Frida Garcia <garciafrida09@gmail.com>
2020-06-10 11:03:39 -0400

    more more files

--- classes_dice_game.py
+++ classes_dice_game.py
@@ -17,7 +17,7 @@ def die(choice):
             sides = random.randint(1, 12)
         elif choice == "d20" or choice == "D20":
             sides = random.randint(1, 20)
-        return(sides)
+        return()
 
 print(die("D6"))
 
@@ -39,59 +39,8 @@ print(die("D6"))
 
 
 
-# class Character():
-
-#     def __init__(self, name, position, speed=10, health=100, attack_power=5):
-#         print("A character was created")
-#         self.name = name
-#         self.health = health
-#         self.speed = speed
-#         self.position = position
-#         self.attack_power = attack_power
 
 
-#     def attack(self, char):
-#         char.health -= self.attack_power
-
-#     def move(self, dir):
-#         if dir == "right":
-#             self.positions["x"] += self.peed
-#         elif dir == "left":
-#             self.position["x"] -= self.speed
-#         elif dir == "up":
-#             self.position["y"] -= self.speed
-#         elif dir == "down":
-#             self.position["y"] += self.speed
-
-# class Player(Character):
-#     def heal(self):
-#         self.health += 25
-
-# class Enemy(Character):
-#     def __init__(self, name, position):
-#         super().__init__(name,position)
-#         self.health = 50
-#         self.speed = 40
-#         print("Enemy has entered the chat")
-#         # self.name = name
-#         # self.health = 50
-#         # self.speed = 40
-#         # self.position = position
-#         # self.attack_power = 68
-
-#     def roll(self):
-#         self.position["x"] -= 25
-
-
-# enemy1 = Enemy("Enemy", {"x":1, "y":100})
-# player1 = Player("Bob", {"x":10, "y":200}, 140, 1000)
-
-# while player1.health > 300:
-#     enemy1.attack(player1)
-#     print(player1.health)
-
-# player1.attack(enemy1)
-# print(enemy1.health)
 
 
 
--- small_function_exc.py
+++ small_function_exc.py
@@ -1,28 +1,28 @@
 
 
-# def mad_lib(name, subject):
-#     try:
-#         print("%s's favorite subject is %s" % (name, subject))
-#     except TypeError:
-#         print('You need to insert a subject')
+def mad_lib(name, subject):
+    try:
+        print("%s's favorite subject is %s" % (name, subject))
+    except TypeError:
+        print('You need to insert a subject')
 
 
-# print(mad_lib('Jen','Math'))
+print(mad_lib('Jen','Math'))
 
 
-# #celsius to Fahrenheit
+#celsius to Fahrenheit
 
-# def c_to_f(c):
-#     f = (c * 9/5) + 32
-#     return f
+def c_to_f(c):
+    f = (c * 9/5) + 32
+    return f
 
-# print(c_to_f(23))
+print(c_to_f(23))
 
-# def is_even(num):
-#     if num % 2 == 0:
-#         print(True)
-#     else:
-#         print(False)
+def is_even(num):
+    if num % 2 == 0:
+        print(True)
+    else:
+        print(False)
 
 
 # is_even(4)
@@ -32,11 +32,11 @@
 
 # only_evens = []
 
-# def only_evens(is_even):
+def only_evens(is_even):
     
-#     return only_evens
+    return only_evens
 
-# print(only_evens([11, 93, 34]))
+print(only_evens([11, 93, 34]))
 
 
 #Write your function here
@@ -54,3 +54,7 @@ def larger_list(lst1, lst2):
 
 #Uncomment the line below when your function is done
 print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+
+def smallest(lst):
+    while len(lst) > 5:
